a5b6a7907e9aee9b18cb9b0e1d83323d0d80b25d
Timothe <hegetimothe@gmail.com>
2023-12-16 19:05:01 +0100

    fusion des css , reajustement des chemin des balise a pour le login et le register

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../style.css" />
+    <link rel="stylesheet" href="../styles.css" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 
@@ -62,7 +62,7 @@
                                 <button type="submit" class="btn btn-primary text-center btn-submit">
                                     Se connecter
                                 </button>
-                                <a class="text-center a-switch " href="../register/index.php">Vous n’avez pas de compte ? <span style=" font-weight: 700; ">Crée un compte ! </span> </a>
+                                <a class="text-center a-switch " href="../register/register.php">Vous n’avez pas de compte ? <span style=" font-weight: 700; ">Crée un compte ! </span> </a>
 
                             </form>
                         </div>
