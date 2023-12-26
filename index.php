69b9bb25fcee33933c6fa2b7b0ee2e02fb5ae8be
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-12-26 16:41:15 +0100

    nouveau

--- index.php
+++ index.php
@@ -7,10 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 
         <link href="https://cdn.jsdelivr.net/npm/remixicon@3.7.0/fonts/remixicon.css" rel="stylesheet" />
-<<<<<<< HEAD
         <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
-=======
->>>>>>> parent of 6060e77 (ajout de la nouvelle branche et ajout du dashboard)
 
         <link rel="stylesheet" href="styles.css" />
         <title>ACF2L</title>
@@ -290,7 +287,6 @@
   </div>
 </div>
 
-<<<<<<< HEAD
 
 <section  id="formulaire">
     <div class="container-fluid">
@@ -374,8 +370,6 @@
     }
 </script>
 
-=======
->>>>>>> parent of 6060e77 (ajout de la nouvelle branche et ajout du dashboard)
 </section>
         <script src="https://unpkg.com/ionicons@latest/dist/ionicons.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
