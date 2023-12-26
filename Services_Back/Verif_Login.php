11831beb7872a4f0421efadff5fe8c1cf58077d7
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-12-26 16:38:58 +0100

    Revert "ajout de la nouvelle branche et ajout du dashboard"
    This reverts commit 6060e77d7b5d5c28f6d56f9f2ea17a5eef4c4f2f.

--- Services_Back/Verif_Login.php
+++ Services_Back/Verif_Login.php
@@ -5,14 +5,10 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
     $email = $_POST["email"];
     $password2 = $_POST["password"];
     $user = new User(0, "", "", $email, $password2, "", "", "");
-    $userId = $user->checkUserExists("ASTA_USER", $email, $password2);
-    if($userId){
-        $_SESSION["user"] = array(
-            "userId" => $userId,
-            "email" => $email,
-            "password" => $password2
-        );
-        $_SESSION["TESTUSER"] = $_SESSION["user"];
+    $user->checkUserExists("ASTA_USER", $email, $password2);
+    if($user->checkUserExists("ASTA_USER", $email, $password2)){
+        $_SESSION["email"] = $email;
+        $_SESSION["password"] = $password2;
         header("Location: ../index.php");
     }else{
         header("Location: ../login-register/login/login.php");
