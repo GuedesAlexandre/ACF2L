6f49811296df45c737b6434be696dd0b86b83738
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-12-28 10:41:50 +0100

    amélioration de la connexion et de l'inscription

--- Services_Back/Verif_Login.php
+++ Services_Back/Verif_Login.php
@@ -4,11 +4,15 @@ session_start();
 if(isset($_POST["email"]) && isset($_POST["password"])){
     $email = $_POST["email"];
     $password2 = $_POST["password"];
+
     $user = new User(0, "", "", $email, $password2, "", "", "");
    
     if($user->checkUserExists("ASTA_USER", $email, $password2)){
-        $_SESSION["user"] = $email;
+      
+        $_SESSION["user"] =  User:: getUserByEmail("ASTA_USER", $email); ;
+        $_SESSION["ID_USER_CONNECTER"]= $firstUserId = $_SESSION["user"][0]->getUserId();
         header("Location: ../index.php");
+        
 
 
         
