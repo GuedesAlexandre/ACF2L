54357add48cb881945299a7c530bdf39a96f1434
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-01-01 17:53:09 +0100

    modif des user

--- Services_Back/adherents.php
+++ Services_Back/adherents.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '/Services_Back/Database.php';
-require_once '/Services_Back/User.php';
+require_once '../Services_Back/Database.php';
+require_once '../Services_Back/User.php';
 
 class Adherents extends User {
 
@@ -10,19 +10,41 @@ class Adherents extends User {
     protected $SITUATION;
     protected $SITUATIONDATE;
     protected $TEL;
-    protected $NB_PERSONNE;
+
     protected $ACTIVITES;
 
-    public function __construct($CIVI, $ADDR, $SITU, $SITUDATE, $TELEPHONE, $NB_PERS, $ACTI, $USER_ID, $NOM, $PRENOM, $EMAIL, $PASSWORD, $BIRTHDATE, $ADRESSE, $role) {
+    public function insertIntADH($tableName){
+        $host = "localhost";
+        $username = "root";
+        $password = "root"; 
+        $database = "ASTA_ACF2L";
+        $db = new Database($host, $username, $password, $database);
+        $db->connect(); // Se connecter à la base de données
+        $connection = $db->connection; // Obtenir la connexion PDO
+        $hashedPassword = password_hash($this->PASSWORD, PASSWORD_DEFAULT); // Hash the password
+        $query = "INSERT INTO $tableName (USER_ID, NOM, PRENOM, EMAIL, PASSWORD, BIRTHDATE, ADRESSE, role, CIVILITE, ADRESSE, SITUATION, SITUATIONDATE, TEL, NB_PERSONNE, ACTIVITES) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $statement = $connection->prepare($query);
+        $success = $statement->execute([$this->USER_ID, $this->NOM, 
+        $this->PRENOM, $this->EMAIL, $hashedPassword, $this->BIRTHDATE, $this->ADRESSE, $this->role, 
+        $this->CIVILITE, $this->ADRESSE, $this->SITUATION, $this->SITUATIONDATE, $this->TEL]); // Use hashed password
+        if ($success) {
+            echo "Adhérents créé";
+        } else {
+            echo "Erreur lors de la création de l'adhérent";
+        }
+    }
+    public function __construct($CIVI, $ADDR, $SITU, $SITUDATE, $TELEPHONE,  $ACTI, $USER_ID, $NOM, $PRENOM, $EMAIL, $PASSWORD, $BIRTHDATE, $ADRESSE, $role) {
         parent::__construct($USER_ID,$NOM, $PRENOM, $EMAIL, $PASSWORD, $BIRTHDATE, $ADRESSE, $role);
         $this -> CIVILITE = $CIVI;
         $this -> ADRESSE = $ADDR;
         $this -> SITUATION = $SITU;
         $this -> SITUATIONDATE = $SITUDATE;
         $this -> TEL = $TELEPHONE;
-        $this -> NB_PERSONNE = $NB_PERS;
+       
         $this -> ACTIVITES = $ACTI;
     }
+
+
     
     public static function getUserInfo($userID) {
         $host = "localhost";
@@ -34,13 +56,15 @@ class Adherents extends User {
         $db->connect(); // Se connecter à la base de données
         $connection = $db->connection; 
 
-        $query = "SELECT * FROM user WHERE USER_ID = :userID";
+        $query = "SELECT * FROM ASTA_USER WHERE USER_ID = :userID";
         $stmt = $connection->prepare($query);
+        
         $stmt->bindParam(':userID', $userID);
+        
         $stmt->execute();
 
-        $userInfo = $stmt->fetch(PDO::FETCH_ASSOC);
-
+        $userInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
         return $userInfo;
     }
             public static function DevAdh($UserID){
@@ -55,19 +79,5 @@ class Adherents extends User {
             }
 
 
-            public function getUserInfoArray($userID) {
-                $userInfo = self::getUserInfo($userID);
-                $userInfoArray = array(
-                    'USER_ID' => $userInfo['USER_ID'],
-                    'NOM' => $userInfo['NOM'],
-                    'PRENOM' => $userInfo['PRENOM'],
-                    'EMAIL' => $userInfo['EMAIL'],
-                    'PASSWORD' => $userInfo['PASSWORD'],
-                    'BIRTHDATE' => $userInfo['BIRTHDATE'],
-                    'ADRESSE' => $userInfo['ADRESSE'],
-                    'role' => $userInfo['role']
-                );
-
-                return $userInfoArray;
-            }
+            
         }
