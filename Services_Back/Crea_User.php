cb70b22a82cb90917a9f2c0ae776797c2031b39c
aroyer1 <arno.royer2004@gmail.com>
2024-01-02 15:44:22 +0100

    bug

--- Services_Back/Crea_User.php
+++ Services_Back/Crea_User.php
@@ -2,13 +2,11 @@
 session_start();
 
 require_once "../Services_Back/User.php";
-$_SESSION["USER_ID"] = $USER->getUserID();
 if(isset($_POST["nom"]) && isset($_POST["prenom"]) && 
 isset($_POST["date"]) && isset($_POST["email"]) && isset($_POST["Adresse"])&&isset($_POST["password"]) && isset($_POST["passwordverifie"])){
     if(strlen($_POST["password"]) > 8 && preg_match('/^(?=.*[A-Z])(?=.*\d)/', $_POST["password"])){
         if($_POST["password"] == $_POST["passwordverifie"]){
             if(User::checkEmailExists("ASTA_USER", $_POST["email"]) == false ){
-            require_once "User.php";
             require_once "User.php";
             $USER = new User($USER_ID,$_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["password"], $_POST["date"], $_POST["Adresse"], 0);
             $USER->insertIntoTable("ASTA_USER");
