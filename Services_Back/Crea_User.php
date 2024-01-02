552b36c9131b0c273a0ab081328c57131017477e
aroyer1 <arno.royer2004@gmail.com>
2024-01-02 15:44:22 +0100

    bug

--- Services_Back/Crea_User.php
+++ Services_Back/Crea_User.php
@@ -2,24 +2,21 @@
 session_start();
 
 require_once "../Services_Back/User.php";
-$_SESSION["USER_ID"] = $USER->getUserID();
 if(isset($_POST["nom"]) && isset($_POST["prenom"]) && 
 isset($_POST["date"]) && isset($_POST["email"]) && isset($_POST["Adresse"])&&isset($_POST["password"]) && isset($_POST["passwordverifie"])){
     if(strlen($_POST["password"]) > 8 && preg_match('/^(?=.*[A-Z])(?=.*\d)/', $_POST["password"])){
         if($_POST["password"] == $_POST["passwordverifie"]){
             if(User::checkEmailExists("ASTA_USER", $_POST["email"]) == false ){
             require_once "User.php";
-            require_once "User.php";
-            $USER = new User(0,$_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["password"], $_POST["date"], $_POST["Adresse"], 0);
+            $USER = new User($USER_ID,$_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["password"], $_POST["date"], $_POST["Adresse"], 0);
             $USER->insertIntoTable("ASTA_USER");
             $_SESSION["USER"] = $USER;
             $_SESSION["USER_ID"] = $USER->getUserID();
-            header("Location: index.php");
-          
+            header("Location: ../index.php");
             exit();     
             }else {
-                
-                echo"<div style='color: red'>Cette email est déjà utilisé</div>";
+                header("location: ../login-register/register/Register.php");
+                echo"l'email est déjà utilisé";
             }
         }
         else{
