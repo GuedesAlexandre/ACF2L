3b8a2aec652132fe7dde61f9ee1f13a0fe753c5a
Timothe <hegetimothe@gmail.com>
2024-01-03 19:37:02 +0100

    ajout root

--- Services_Back/adherents.php
+++ Services_Back/adherents.php
@@ -17,7 +17,7 @@ class Adherents extends User {
     public function insertIntADH($tableName){
         $host = "localhost";
         $username = "root";
-        $password = ""; 
+        $password = "root";  
         $database = "ASTA_ACF2L";
         $db = new Database($host, $username, $password, $database);
         $db->connect(); // Se connecter à la base de données
@@ -49,7 +49,7 @@ class Adherents extends User {
     public static function getUserInfo($userID) {
         $host = "localhost";
         $username = "root";
-        $password = ""; 
+        $password = "root";  
         $database = "ASTA_ACF2L";
         $db = new Database($host, $username, $password, $database);
 
