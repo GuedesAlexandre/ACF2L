80f0d4cb4e08afeb2d7a842ae7e1b65c39da9953
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-01-04 13:49:49 +0100

    pofinnage

--- page-reservation.php
+++ page-reservation.php
@@ -18,8 +18,6 @@
     <title>Document</title>
 </head>
 
-<body>
-
 
     <section id="reservation-page">
     <header class="container-fluid">
@@ -67,7 +65,6 @@
                                     <select class="d-flex align-items-center input-control form-control-mo select" name="pilote" id="pilote" style="margin-bottom: 21px; padding: 20px">
                                     <option value="select">-- SÉLÉCTIONNEZ UN PILOTE --</option>
     
-                                    <?php
                                     <?php
                                         $servername = "localhost";
                                         $username = "root";
@@ -160,6 +157,6 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-</body>
+
 
 </html>
