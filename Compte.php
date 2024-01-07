648f75d661eda6df671df7d43e25cb97b3b804f6
aroyer1 <arno.royer2004@gmail.com>
2024-01-07 20:45:28 +0100

    Correction

--- Compte.php
+++ Compte.php
@@ -7,7 +7,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    <title>Mon compte | AFC2L</title>
 </head>
 
 <body style="background-color: #090a0c; font-family: 'Inter', sans-serif;">
@@ -137,11 +137,11 @@ if(Adherents::checkADHExists($_SESSION["USER_ID"]) ){
                         <thead class="txt-info-ligne">
                             <tr>
 
-                                <th class="txt-info-item-ligne py-3" scope="col">Nom pilote</th>
-                                <th class="txt-info-item-ligne py-3" scope="col">DATE/HORAIRE</th>
+                                <th class="txt-info-item-ligne py-3" scope="col">Nom du pilote</th>
+                                <th class="txt-info-item-ligne py-3" scope="col">Date & Horraire</th>
                                 <th class="txt-info-item-ligne py-3" scope="col">Date de naissance</th>
-                                <th class="txt-info-item-ligne py-3" scope="col">DESCRIPTION</th>
-                                <th class="txt-info-item-ligne py-3" scope="col">TYPE D'AVION</th>
+                                <th class="txt-info-item-ligne py-3" scope="col">Description</th>
+                                <th class="txt-info-item-ligne py-3" scope="col">Type d'avion</th>
                             </tr>
                         </thead>
                         <tbody>
--- devenir-adeherent.php
+++ devenir-adeherent.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.7.0/fonts/remixicon.css" rel="stylesheet" />
    
-    <title>Document</title>
+    <title>Devenir adhérent | AFC2L</title>
 </head>
 
 
